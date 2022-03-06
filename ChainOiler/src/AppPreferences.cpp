7a942fbd229afadbc56bac114065927521c52f0a
Dirk Marciniak <dirk.marciniak@mail.de>
2022-03-06 16:34:41 +0100

    zähler aktualisiert

--- ChainOiler/src/AppPreferences.cpp
+++ ChainOiler/src/AppPreferences.cpp
@@ -4,7 +4,7 @@
 
 namespace Prefs
 {
-  const char *Preferences::serialStr = "20220226-181650-build-0869";
+  const char *Preferences::serialStr = "20220306-163346-build-0871";
   const std::string Preferences::serialString = std::string(Preferences::serialStr);
   const char *Preferences::tag{"Preferences"};                              //! tag fürs debug logging
   nvs_handle_t Preferences::nvs_handle{0U};                                 //! handle für NVS
