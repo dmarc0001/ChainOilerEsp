5f515262ed4fed3e4e9cb12b04646ca99e525348
Dirk Marciniak <dirk.marciniak@mail.de>
2022-03-16 21:47:03 +0100

    wieder entfernt

--- CrashTest/README.md
+++
@@ -1,6 +0,0 @@
-# Enhanced Chain Oiler like a McCoi Oiler for Motorcycles
-
-...in progress...
-
-description comming soon
-
--- CrashTest/include/main.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_sleep.h"
-#include "esp_log.h"
-#include "ProjectDefaults.hpp"
-
-
-extern "C" void app_main();
